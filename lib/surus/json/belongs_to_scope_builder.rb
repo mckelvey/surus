3ef38f2e83334b211960942343bd46f123b2d829
David W. McKelvey <david@mckelveycreative.com>
2015-08-08 23:16:28 -0700

    adding joins back

--- lib/surus/json/belongs_to_scope_builder.rb
+++ lib/surus/json/belongs_to_scope_builder.rb
@@ -4,6 +4,7 @@ module Surus
       def scope
         s = association
           .klass
+          .joins("JOIN #{join_table} ON #{join_table}.#{association_foreign_key}=#{association_table}.#{association_primary_key}")
           .where("#{outside_class.quoted_table_name}.#{association_primary_key}=#{join_table}.#{foreign_key}")
         s = s.instance_eval(&association.scope) if association.scope
         s
@@ -13,8 +14,16 @@ module Surus
         connection.quote_table_name association.join_table
       end
 
+      def association_foreign_key
+        connection.quote_column_name association.association_foreign_key
+      end
+
       def foreign_key
         connection.quote_column_name association.foreign_key
+      end
+
+      def association_table
+        connection.quote_table_name association.klass.table_name
       end
 
       def association_primary_key
