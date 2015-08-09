2ca5cc6906de07d22512eb87b467882614fe684d
David W. McKelvey <david@mckelveycreative.com>
2015-08-08 23:03:55 -0700

    extending table names to belongs_to associations

--- lib/surus/json/belongs_to_scope_builder.rb
+++ lib/surus/json/belongs_to_scope_builder.rb
@@ -4,10 +4,23 @@ module Surus
       def scope
         s = association
           .klass
-          .where("#{quote_column_name association.active_record_primary_key}=#{quote_column_name association.foreign_key}")
+          .where("#{outside_class.quoted_table_name}.#{association_primary_key}=#{join_table}.#{foreign_key}")
         s = s.instance_eval(&association.scope) if association.scope
         s
       end
+
+      def join_table
+        connection.quote_table_name association.join_table
+      end
+
+      def association_foreign_key
+        connection.quote_column_name association.association_foreign_key
+      end
+
+      def foreign_key
+        connection.quote_column_name association.foreign_key
+      end
+
     end
   end
 end
