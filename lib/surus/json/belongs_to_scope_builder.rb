91ee96980d6ee37fde49755d5eee2db1c2448617
David W. McKelvey <david@mckelveycreative.com>
2015-08-09 23:20:01 -0700

    potential final

--- lib/surus/json/belongs_to_scope_builder.rb
+++ lib/surus/json/belongs_to_scope_builder.rb
@@ -2,35 +2,27 @@ module Surus
   module JSON
     class BelongsToScopeBuilder < AssociationScopeBuilder
       def scope
-        logger = Rails.logger
-        logger.info "current table"
-        logger.info outside_class.quoted_table_name
-        
         s = association
           .klass
-          .where("#{quote_column_name association.active_record_primary_key}=#{association_table}.#{quote_column_name association.foreign_key}")
+          .where("#{primary_table}.#{primary_key}=#{association_table}.#{association_foreign_key}")
         s = s.instance_eval(&association.scope) if association.scope
         s
       end
 
-      def join_table
-        connection.quote_table_name association.join_table
+      def primary_table
+        outside_class.quoted_table_name
       end
 
-      def association_foreign_key
-        connection.quote_column_name association.association_foreign_key
-      end
-
-      def foreign_key
-        connection.quote_column_name association.foreign_key
+      def primary_key
+        quote_column_name association.active_record_primary_key
       end
 
       def association_table
         connection.quote_table_name association.klass.table_name
       end
 
-      def association_primary_key
-        connection.quote_column_name association.association_primary_key
+      def association_foreign_key
+        quote_column_name association.foreign_key
       end
 
     end
